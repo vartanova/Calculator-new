da79476ddb772cf313b6014e12e5291ccc0b27ab
vartanova <vartanova.fortech@gmail.com>
2026-03-15 01:21:00 +0300

    add electron

---
+++ main.js
@@ -0,0 +1,24 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile(path.join(__dirname, "dist", "index.html"));
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
